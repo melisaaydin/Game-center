ea8bfb476f8dab21694366d0d033c7eb7fa1fc5c
melisaaydin <melisa.aydin650@gmail.com>
2025-04-08 13:28:03 +0300

    added some codes

--- packages/game-center/src/components/FeaturedGames/FeaturedGames.jsx
+++ packages/game-center/src/components/FeaturedGames/FeaturedGames.jsx
@@ -8,12 +8,13 @@ const FeaturedGames = () => {
     const { mode } = useContext(ColorModeContext);
 
     return (
+        //grid for home-page
         <div className="grid-property">
             <div className="container">
                 <div className="item-a">PVP Game</div>
                 <div className="item-b">Roulette</div>
                 <div className="item-c">Jackpot</div>
-                <div className="item-d">Slots</div>
+                <div className="item-d" >Slots</div>
                 <div className="item-e">Cases</div>
                 <div className="item-f">Crash</div>
             </div>
